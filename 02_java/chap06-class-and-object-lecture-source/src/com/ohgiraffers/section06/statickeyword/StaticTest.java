8ceaede27f8b11e5d0e96ef354fe49f52a678d38
1214alex <jerry20011214@gmail.com>
2025-02-03 19:35:27 +0900

    객체를 이용한 배열까지

---
+++ 02_java/chap06-class-and-object-lecture-source/src/com/ohgiraffers/section06/statickeyword/StaticTest.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section06.statickeyword;
+
+public class StaticTest {
+    private int nonStaticCount;
+    private static int staticCount;
+
+    public StaticTest() {
+
+    }
+
+    public int getNonStaticCount() {
+        return nonStaticCount;
+    }
+
+    public static int getStaticCount() {
+        return staticCount;
+    }
+    public void increaseNonstaticCount(){
+        this.nonStaticCount++;
+    }
+    public static void increasestaticCount(){
+        StaticTest.staticCount++;
+    }
+}
